46ad4bfb836897dea8e2246fd97b9d36605f0464
Ermes Liriano <ermesliriano@mercedarios.net>
2024-04-21 14:54:09 +0200

    Expansion del resto de las operaciones aritmeticas: resta, multiplicacion y division con gestion de excepciones

--- operaciones.py
+++ operaciones.py
@@ -1,5 +1,19 @@
 def sumar(a, b):
-     return a + b
+    return a + b
+
+def restar(a, b):
+    return a - b
+
+def multiplicar(a, b):
+    return a * b
+
+def dividir(a, b):
+    if b == 0: # Si el divisor es cero, se levanta una excepcion de la clase ValueError
+        raise ValueError("No se puede dividir por cero")
+    return a / b
 
 if __name__ == "__main__":
-     print(sumar(5, 3))
+    print(sumar(5, 3))
+    print(restar(5, 3))
+    print(multiplicar(5, 3))
+    print(dividir(5, 3))
--- test_operaciones.py
+++ test_operaciones.py
@@ -1,9 +1,31 @@
 import unittest
-from operaciones import sumar
+from operaciones import sumar, restar, multiplicar, dividir
 
-class TestSumar(unittest.TestCase):
+class TestOperaciones(unittest.TestCase):
 
-     def test_sumar(self):
-          self.assertEqual(sumar(3, 2), 5)
-          self.assertEqual(sumar(-1, 1), 0)
-          self.assertEqual(sumar(-1, -1), -2)
+    def test_sumar(self):
+        self.assertEqual(sumar(3, 2), 5)
+        self.assertEqual(sumar(-1, 1), 0)
+        self.assertEqual(sumar(-1, -1), -2)
+
+    def test_restar(self):
+        self.assertEqual(restar(3, 2), 1)
+        self.assertEqual(restar(-1, 1), -2)
+        self.assertEqual(restar(-1, -1), 0)
+
+    def test_multiplicar(self):
+        self.assertEqual(multiplicar(3, 2), 6)
+        self.assertEqual(multiplicar(-1, 1), -1)
+        self.assertEqual(multiplicar(-1, -1), 1)
+
+    def test_dividir(self):
+        self.assertEqual(dividir(8, 2), 4)
+        self.assertEqual(dividir(-4, 2), -2)
+        self.assertEqual(dividir(5, -1), -5)
+
+    def test_dividir_por_cero(self):
+        with self.assertRaises(ValueError): #Con esta expresion, se manejan y prueban excepciones ValueError
+            dividir(5, 0)
+
+if __name__ == '__main__':
+    unittest.main()
